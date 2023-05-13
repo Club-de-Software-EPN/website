4f5484b79c69d7ec0ee5d161b7084da1b9685c4c
DebianH <davidhernandez_s@hotmail.com>
2023-05-12 19:06:17 -0500

    add routes in vitejs

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        teams: resolve(__dirname, 'equipos.html')
+        teams: resolve(__dirname, 'equipos.html'),
+        proyect: resolve(__dirname, 'proyect.html'),
+        register: resolve(__dirname, 'indexRegister.html'),
       }
     }
   }
